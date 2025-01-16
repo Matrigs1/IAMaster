0f6a3efa427be09bca276572b6c3f8df0d882a24
Mateus Rodrigues Lima da Silva <mrlsilva@tecnocomp.msft>
2025-01-16 16:17:23 -0300

    Atualizando main, criando requirements do projeto e, subindo o gitignore.

---
+++ main.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+
+# Autenticação da conta.
+client = OpenAI(
+    api_key='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',
+)
+
+message = client.chat.completions.create(
+    model = 'gpt-3.5-turbo', # Modelo do chat.
+    # Prompt como usuário.
+    messages = [
+        {
+            'role': 'system', 'content': 'Dê respostas técnicas sobre programação. Se comporte como um programador Python experiente especialista em padrões de projetos e arquitetura limpa.' # Dando um contexto a IA, que irá retornar informações mais apuradas.
+        },
+        {
+            'role': 'user',
+            'content': 'Me mostre como posso fazer um projeto Django com as melhores boas práticas.'
+        }
+    ],
+    stream = False # Automaticamente é False, o que faz com que a resposta seja gerada e, somente enviada após completamente formulada.
+)
+
+print(message.choices[0].message.content)
+
+# # A stream retorna continuamente porções de resposta, que podem ser iteradas e retornadas, assim como no chat GPT.
+# for chunk in stream:
+#     if chunk.choices[0].delta.content is not None:
+#         print(chunk.choices[0].delta.content, end = '')
